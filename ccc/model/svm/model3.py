b4e09c6f91188cfa9c68426ee90132065b7032f6
katakd <89955319+katakd@users.noreply.github.com>
2022-05-31 00:26:14 +0700

    update svm classifier and 5 models

---
+++ ccc/model/svm/model3.py
@@ -0,0 +1,11 @@
+import svm
+
+filename = "clean_data.csv"
+model1 = svm.SVM()
+model1.getDataset(filename)
+model1.dataPrepare(test_size= 0.2,dimention_reduct=True,k=28, feature_select=True, f_type = ('sfm',0.12), encoding = 21, oversampling=True)
+model1.train(15,'auto')
+model1.predict()
+model1.confusion_matrix(True)
+print(model1.accuraccy())
+print(model1.f_score(0))
