1e0fd82b412dcd64e7b2f83e5c63a5f314c8db5d
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-06-19 19:05:02 +0700

    FINISH DATA SPLITTING, add guide

--- ccc/models/linreg/linreg.py
+++ ccc/models/linreg/linreg.py
@@ -1,13 +1,20 @@
 
+import os
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 
+ABS_PATH = os.path.abspath(os.path.dirname(__file__))
 
+X_train = pd.read_csv(os.path.join(ABS_PATH, 'X_train.csv')).to_numpy()
+y_train = pd.read_csv(os.path.join(ABS_PATH, 'y_train.csv')).to_numpy()
+X_test = pd.read_csv(os.path.join(ABS_PATH, 'X_test.csv')).to_numpy()
+y_test = pd.read_csv(os.path.join(ABS_PATH, 'y_test.csv')).to_numpy()
 
-X_train, y_train, X_test, y_test = get_train_test_onehot_df()
+print(X_test, y_test)
 
+'''
 linreg = LinearRegression().fit(X_train, y_train)
 # print(linreg.coef_)
 
@@ -23,3 +30,4 @@ print(y_pred)
 print(pd.Series.value_counts(y_pred))
 
 print(f'acc = {accuracy_score(y_test, y_pred)}')
+'''
